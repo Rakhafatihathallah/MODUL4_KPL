373db9841d8fb28a0834956111b83c7c95da67e7
Fakbar11 <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:25:29 +0700

    Menambahkan class kodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace modul4_103022300130
+{
+    public class KodeProduk
+
+    {
+        private static Dictionary<string, string> listProduk = new Dictionary<string, string>
+        {
+            {"Laptop","E100"},
+            {"Smartphone","E101"},
+            {"Tablet","E102"},
+            {"Headset","E103"},
+            {"Keyboard","E104"},
+            {"Mouse","E105"},
+            {"Printer","E106"},
+            {"Monitor","E107"},
+            {"Smartwatch","E108"},
+            {"Kamera","E109"}
+        };
+
+        public static string getKodeProduk(string namaProduk)
+        {
+            if (listProduk.TryGetValue(namaProduk, out string kodeProduk))
+            {
+                return kodeProduk;
+            } else
+            {
+                return "Tidak dapat menemukan kode produk yang dicari";
+            }
+        }
+    }
+}
---
+++ MainProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using modul4_103022300130;
+
+public class MainProgram()
+{
+    public static void Main(string[] args) 
+    {
+        string namaProduk = Console.ReadLine();
+       
+        Console.WriteLine(KodeProduk.getKodeProduk(namaProduk));
+    }
+}
